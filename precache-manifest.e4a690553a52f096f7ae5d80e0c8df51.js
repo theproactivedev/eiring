ab531a68b5a91e0d31cb882452db6d5182177fe5
Eirin <eiringonzales@gmail.com>
2020-05-21 22:24:22 +0800

    Updates

---
+++ precache-manifest.e4a690553a52f096f7ae5d80e0c8df51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6239e3d19187422d1f98a83fefcf94c2",
+    "url": "/eiring/index.html"
+  },
+  {
+    "revision": "d0093338d1880ea6fb28",
+    "url": "/eiring/static/css/main.89adfc5a.chunk.css"
+  },
+  {
+    "revision": "33f9680a8a6a90ef3048",
+    "url": "/eiring/static/js/2.65c14bab.chunk.js"
+  },
+  {
+    "revision": "570d10197340f0c06eb0c25537fab92d",
+    "url": "/eiring/static/js/2.65c14bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0093338d1880ea6fb28",
+    "url": "/eiring/static/js/main.7b3d62a4.chunk.js"
+  },
+  {
+    "revision": "6c6f685d267af7d329b1",
+    "url": "/eiring/static/js/runtime-main.ebe75c40.js"
+  }
+]);
